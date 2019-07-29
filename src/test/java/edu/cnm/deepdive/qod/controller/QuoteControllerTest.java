dfd5a5d03748d29cc6fffb564abc629274e54260
Terrell Page <tpage46@gmail.com>
2019-07-29 10:34:29 -0600

    created test methods

--- src/test/java/edu/cnm/deepdive/qod/controller/QuoteControllerTest.java
+++ src/test/java/edu/cnm/deepdive/qod/controller/QuoteControllerTest.java
@@ -2,20 +2,20 @@ package edu.cnm.deepdive.qod.controller;
 
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
-import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
-import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import edu.cnm.deepdive.qod.SpringRestDocsApplication;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -46,9 +46,25 @@ class QuoteControllerTest {
         .andDo(document("get-random",
             preprocessRequest(prettyPrint()),
             preprocessResponse(prettyPrint()),
-//            links(linkWithRel("self").description("URL of quote")),
             responseHeaders(headerWithName("Content-Type").description("Content type of payload"))
         ));
   }
 
+  @Test
+  void get404() throws Exception {
+    mockMvc.perform(get("/quotes/11111111-1111-1111-1111-1111-111111111111"))
+        .andExpect(MockMvcResultMatchers.status().isNotFound());
+  }
+
+  @Test
+  void postQuote() throws Exception {
+    mockMvc.perform(
+        post("/quotes")
+        .content("{\"text\" \"Be nice.\"}")
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON)
+        )
+    .andExpect(MockMvcResultMatchers.status().isCreated())
+    .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+  }
 }
